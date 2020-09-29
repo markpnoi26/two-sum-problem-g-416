42157b812f57d2dddf49b871f7b85076da497099
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-09-29 21:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ function bruteForceTwoSum(sum, a) {
   return ans
   
 }
+
+function binarySearchTwoSum(sum, a) {
+  
+  const hash = {}, ans = []
+  
+  for (let num of a) {
+    hash[num] = hash[num]+1 || 1
+  }
+  
+  
+  for (let num in hash) {
+    hash[num]--
+    if (hash[sum - num] > 0) {
+      ans.push([num, sum - num])
+      hash[sum - num]--
+    }
+  }
+  
+  return ans
+  
+}

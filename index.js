7f23c647a7bc4aa1687fbfd9cc12b01e9caf5612
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-09-29 21:19:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function twoSum(sum, a) {
+  
+  const hash = {}, ans = []
+  
+  for (let num of a) {
+    hash[num] = hash[num]+1 || 1
+  }
+  
+  
+  for (let num in hash) {
+    hash[num]--
+    if (hash[sum - num] > 0) {
+      ans.push([num, sum - num])
+      hash[sum - num]--
+    }
+  }
+  
+  return ans
+  
+}
